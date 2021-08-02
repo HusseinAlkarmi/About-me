8d1d45468116814ade218e40aa8e0e9be21862a9
husseinalkarmi <husseinalkarmi012@gmail.com>
2021-08-02 15:45:18 +0300

    js

---
+++ JSF/app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+
+let age = Number(prompt("What is my age?"))
+
+let darker = prompt("Black is darker than white right for me ?")
+let cigarette = prompt("Is Winston my favorite cigarette ?")
+let pinkColor = prompt("Is Pink really a Mahre ?")
+let spongeBob = prompt("Is SpongeBob my favorite character ?")
